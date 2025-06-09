74d5901cd658fdd0ef958e3d4c7da90ff2e989c8
Mkiskrazy <mahihaasan123@gmail.com>
2025-06-09 18:28:23 +0530

    added react helmet to pages

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,16 @@
 import BannerCard from "../../features/profile/BannerCard";
 import ServicesContainer from "../../features/services/ServicesContainer";
+import { Helmet } from 'react-helmet';
 
 function Home() {
   return (
     <div className="mx-auto mt-8 flex w-full flex-col justify-center px-4">
+      <Helmet>
+            <meta charSet="utf-8" />
+            <title>Hexpertify-Online Consulting</title>    
+            <meta name="description" content="Hexpertify consulting Platform" />
+            <meta name="keywords" content="Online Consulting, Hexpertify, Consulting, About Hexpertify" />
+      </Helmet>
       <BannerCard />
       <div className="sm:px-2">
         <h1 className="py-4 text-start font-Alata text-2xl font-normal sm:text-center sm:text-5xl">
