2a8ce18f0075f581a5a9711024e40dd994d11395
Hexpertify <hexpertifyapp@gmail.com>
2025-07-09 11:39:07 +0530

    Update index.jsx

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -7,15 +7,16 @@ function Home() {
     <div className="mx-auto mt-8 flex w-full flex-col justify-center px-4">
       <Helmet>
             <meta charSet="utf-8" />
-            <title>Hexpertify-Online Consulting</title>    
+            <title>Hexpertify-Consult CERTIFIED Experts Online</title>    
             <meta name="description" content="Hexpertify consulting Platform" />
             <meta name="keywords" content="Online Consulting, Hexpertify, Consulting, About Hexpertify" />
       </Helmet>
       <BannerCard />
       <div className="sm:px-2">
         <h1 className="py-4 text-start font-Alata text-2xl font-normal sm:text-center sm:text-5xl">
-          Featured Services
+          Consult Certified Experts from Doctors to Fashion advisors | Hexpertify
         </h1>
+        <h2 className="mt-6 text-xl font-semibold sm:text-3xl">Featured Services</h2>
         <ServicesContainer />
       </div>
     </div>
