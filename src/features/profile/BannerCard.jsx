1d43d5cd3535999befedd2202abdffa1c3568587
Hexpertify <hexpertifyapp@gmail.com>
2025-07-09 11:45:55 +0530

    Update BannerCard.jsx

--- src/features/profile/BannerCard.jsx
+++ src/features/profile/BannerCard.jsx
@@ -6,7 +6,6 @@ function BannerCard() {
   return (
     <div className="relative mb-6 px-2 sm:px-8">
       <div className="absolute inset-0 flex items-center justify-center rounded-3xl bg-opacity-50 p-4 text-center">
-        <h1 className="absolute inset-0 m-auto flex h-32 w-64 items-center justify-center font-Akshar text-4xl font-semibold text-white sm:text-5xl md:text-6xl lg:text-7xl"></h1>
       </div>
       <img
         src={banner ?? "./background.jpg"}
