7e9c77ee9d88f0e8b40fd0f9f6534f4dab31f9cc
Hexpertify <hexpertifyapp@gmail.com>
2025-07-14 15:47:48 +0530

    Google Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from "react-router-dom";
-
 import QueryProvider from "./lib/tanstackQuery";
 import { router } from "./routes";
-import AuthProvider from "./contexts/AuthContext ";
+import AuthProvider from "./contexts/AuthContext";
 import Notification from "./components/notification";
+import useGAPageView from "./hooks/useGAPageView";
 
 function App() {
+  useGAPageView();
+
   return (
     <AuthProvider>
       <QueryProvider>
