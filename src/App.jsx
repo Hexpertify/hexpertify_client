f51516b7f9e4dbe7c703d4aa16915d53babde730
Hexpertify <hexpertifyapp@gmail.com>
2025-07-14 20:59:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { RouterProvider } from "react-router-dom";
+
 import QueryProvider from "./lib/tanstackQuery";
 import { router } from "./routes";
-import AuthProvider from "./contexts/AuthContext.jsx ";
+import AuthProvider from "./contexts/AuthContext ";
 import Notification from "./components/notification";
-import useGAPageView from "./hooks/useGAPageView";
 
 function App() {
-  useGAPageView();
-
   return (
     <AuthProvider>
       <QueryProvider>
